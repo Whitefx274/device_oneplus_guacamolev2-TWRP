aac59b87e9ed6c2f479ce7c8fa2673d3926c4302
Whitefx274 <115898299+Whitefx274@users.noreply.github.com>
2024-02-16 00:56:15 +0000

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/nebrassy/device_oneplus_sm8150-common-TWRP -b android-12.1 device/oneplus/sm8150-common
+git clone https://github.com/nebrassy/android_kernel_oneplus_sm8150 -b android-12.1 kernel/oneplus/sm8150
